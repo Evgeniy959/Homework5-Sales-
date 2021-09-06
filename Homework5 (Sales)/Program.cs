9cb58160c913dcf3b6301626ef87c3ea9f3e5c2e
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-09-06 14:15:26 +0700

    Add project files.

---
+++ Homework5 (Sales)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework5__Sales_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
